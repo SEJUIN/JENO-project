8ba0e3eb6953c915cf0462e98107f9552e65c7c0
SEJUIN <80575726+SEJUIN@users.noreply.github.com>
2021-05-11 04:43:57 +0900

    Main UI Setting

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject mainMenu;
+    [SerializeField]
+    GameObject stageMenu;
+
+    void Awake()
+    {
+        mainMenu.SetActive(true);
+        stageMenu.SetActive(false);
+    }
+
+    public void StageMode()
+    {
+        mainMenu.SetActive(false);
+        stageMenu.SetActive(true);
+        //SceneManager.LoadScene("StageMode");
+    }
+    public void InfiniteMode()
+    {
+        SceneManager.LoadScene("InfiniteMode");
+    }
+
+    public void Exit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+}
